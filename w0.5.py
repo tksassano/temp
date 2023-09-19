0f1796ae98a02125722af1cb9d6a6eccd5f082d5
tksassano <113140378+tksassano@users.noreply.github.com>
2023-09-19 14:43:55 -0400

    Update w0.5.py

--- w0.5.py
+++ w0.5.py
@@ -1,45 +1,43 @@
-import math
+from math import dist
 
-def lineFromPoints(p1, p2):
-    m = (p2[1]-p1[1]) / (p2[0]-p1[0])
-    b = p2[1] - m * p2[0]
-    def evaluate(x):
-        return int(m * x + b)
-    return m, evaluate
-
-def getCandidates(f, m, p1, p2):
-    x, y = p1
-    if m > 0:
-        candidates = [(x+1, y+1), (x+1, y), (x, y+1)]
-    elif m < 0:
-        candidates = [(x+1, y-1), (x+1, y), (x, y-1)]
-    elif m == 0:
-        candidates = [(x+1, y), (x-1, y)]
-    else: 
-        candidates = [(x, y+1), (x, y-1)]
-    return candidates
-
-def determineBest(f, m, p1, p2):
-    x, y = p1
-    candidates = getCandidates(f, m, p1, p2)
-    closest = candidates[0]
-    for p in candidates:
-        d = math.dist(p, p2)
-        if d < math.dist(closest, p2):
-            closest = p
-    return closest
-
-def createLine(f, m, p1, p2):
-    points = [p1]
-    while p1 != p2:
-        p1 = determineBest(f, m, p1, p2)
-        points.append(p1)
-    return points
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+        self.b = p2[1] - self.m * p2[0]
+        
+    def evaluate(self, x):
+        return int(self.m * x + self.b)
+    
+    def getCandidates(self, point):
+        x, y = point
+        if self.m > 0:
+            return [(x+1, y+1), (x+1, y), (x, y+1)]
+        elif self.m < 0:
+            return [(x+1, y-1), (x+1, y), (x, y-1)]
+        elif self.m == 0:
+            return [(x+1, y), (x-1, y)]
+        else:
+            return [(x, y+1), (x, y-1)]
+    
+    def determineBest(self, point):
+        candidates = self.getCandidates(point)
+        closest = candidates[0]
+        for p in candidates:
+            d = dist(p, self.p2)
+            if d < dist(closest, self.p2):
+                closest = p
+        return closest
+    
+    def createLine(self):
+        points = [self.p1]
+        while self.p1 != self.p2:
+            self.p1 = self.determineBest(self.p1)
+            points.append(self.p1)
+        return points
 
 p1 = (2, 1)
 p2 = (5, 10)
-
-m, f = lineFromPoints(p1, p2)
-
-line = createLine(f, m, p1, p2)
-print(line)
+line = Line(p1, p2)
+print(line.createLine())
