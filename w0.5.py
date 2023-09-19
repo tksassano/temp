32eed7ab307077a8a2d865aed8e1d2e001f14c52
tksassano <113140378+tksassano@users.noreply.github.com>
2023-09-19 14:33:43 -0400

    Update w0.5.py

--- w0.5.py
+++ w0.5.py
@@ -5,12 +5,23 @@ def lineFromPoints(p1, p2):
     b = p2[1] - m * p2[0]
     def evaluate(x):
         return int(m * x + b)
-    return evaluate
+    return m, evaluate
 
-def determineBest(f, p1, p2):
-    x = p1[0]
-    y = p1[1]
-    candidates = [(x+1, y+1), (x-1, y-1), (x+1, y-1), (x-1, y+1)]
+def getCandidates(f, m, p1, p2):
+    x, y = p1
+    if m > 0:
+        candidates = [(x+1, y+1), (x+1, y), (x, y+1)]
+    elif m < 0:
+        candidates = [(x+1, y-1), (x+1, y), (x, y-1)]
+    elif m == 0:
+        candidates = [(x+1, y), (x-1, y)]
+    else: 
+        candidates = [(x, y+1), (x, y-1)]
+    return candidates
+
+def determineBest(f, m, p1, p2):
+    x, y = p1
+    candidates = getCandidates(f, m, p1, p2)
     closest = candidates[0]
     for p in candidates:
         d = math.dist(p, p2)
@@ -18,18 +29,18 @@ def determineBest(f, p1, p2):
             closest = p
     return closest
 
-def createLine(p1, p2, f):
+def createLine(f, m, p1, p2):
     points = [p1]
 
     while p1 != p2:
-        p1 = determineBest(f, p1, p2)
+        p1 = determineBest(f, m, p1, p2)
         points.append(p1)
     
     return points
 
 p1 = (2, 1)
 p2 = (5, 10)
-f = lineFromPoints(p1, p2)
+m, f = lineFromPoints(p1, p2)
 
-line = createLine(p1, p2, f)
+line = createLine(f, m, p1, p2)
 print(line)
