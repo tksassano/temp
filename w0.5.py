8c7ad1f7844f38d1cfbefe345108f1bba92bfdbb
tksassano <113140378+tksassano@users.noreply.github.com>
2023-09-19 20:14:59 -0400

    Update w0.5.py

--- w0.5.py
+++ w0.5.py
@@ -4,10 +4,7 @@ class Line:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        if p1[0] == p2[0]:
-            self.m = None
-        else:
-            self.m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+        self.m = (p2[1] - p1[1]) / (p2[0] - p1[0])
         self.b = p2[1] - self.m * p2[0]
         
     def evaluate(self, x):
@@ -19,10 +16,6 @@ class Line:
             return [(x+1, y+1), (x+1, y), (x, y+1)]
         elif self.m < 0:
             return [(x+1, y-1), (x+1, y), (x, y-1)]
-        elif self.m == 0:
-            return [(x+1, y), (x-1, y)]
-        else:
-            return [(x, y+1), (x, y-1)]
     
     def determineBest(self, point):
         candidates = self.getCandidates(point)
@@ -41,6 +34,6 @@ class Line:
         return points
 
 p1 = (2, 1)
-p2 = (2, 10)
+p2 = (5, 10)
 line = Line(p1, p2)
 print(line.createLine())
